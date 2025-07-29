0b7046f8cfe049f6599ef008a57d6f95cfbe3dea
Rana Sami Ul Haq <rsami3638@gmail.com>
2025-07-29 22:02:20 +0500

    .

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from pymongo import MongoClient
 from fastapi.middleware.cors import CORSMiddleware
 from bson import ObjectId
 from Routes.pluginRouter import router as pluginRouter
+
 app = FastAPI()
-app.include_router(pluginRouter,prefix='/plugin')
+app.include_router(pluginRouter, prefix='/plugin')
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,3 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Add this block so we can run via "python main.py"
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
